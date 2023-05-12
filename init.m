5b212bdf57340133197d2a98ca74b35ad6b5b9b8
AwesomePaneer <rajnikunj2002@gmail.com>
2023-05-12 23:55:07 +0530

    first commit

---
+++ init.m
@@ -0,0 +1,29 @@
+clear all;
+
+% Initialization variables from paper (all in SI units, temperatures in degree celcius)
+E_M = 28e9;
+E_A = 75e9;
+A_s = 88;
+A_f = 98;
+M_s = 72;
+M_f = 62;
+m_w = 6.8e-4;
+R = 20;
+l_0 = 0.37;
+b_s = 0.5;
+k_s = 0.0018;
+C_A = 10e6;
+C_M = 10e6;
+T_amb = 25;
+A = 4.9e-8;
+A_w = 290.45e-6;
+c_p = 320;
+epsilon_L = 0.023;  % = (2.3%)
+h_0 = 20;
+h_2 = 0.001;
+b1 = 0.1;
+b2 = 0.1;
+Theta = -0.055;
+
+r = 7.5e-3;
+I = 2.7e-6; % inertia (=order of mr^2)
